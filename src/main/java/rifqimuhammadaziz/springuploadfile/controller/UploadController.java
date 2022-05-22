56b49f01b2fd1542c9641bbfe048ef5f5ef22682
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-05-22 11:30:53 +0700

    Add path & uploadFile handler, update view

--- src/main/java/rifqimuhammadaziz/springuploadfile/controller/UploadController.java
+++ src/main/java/rifqimuhammadaziz/springuploadfile/controller/UploadController.java
@@ -2,12 +2,48 @@ package rifqimuhammadaziz.springuploadfile.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class UploadController {
 
+    private static String UPLOADED_PATH = "D:/upload/";
+
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file")MultipartFile file, RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload!");
+            return "redirect:status";
+        }
+
+        try {
+            byte[] bytes = file.getBytes();
+            // Path to save file
+            Path path = Paths.get(UPLOADED_PATH + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message", "File successfully uploaded " + file.getOriginalFilename());
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return "redirect:status";
+    }
+
+    @GetMapping("/status")
+    public String uploadStatus() {
+        return "status";
     }
 }
